958de80aadc88c607058e88d0eb215318586e6f4
Tetereka <74609981+Tetereka@users.noreply.github.com>
2021-06-05 11:05:44 +0300

    Коммит с сортировочкой)
    Добавил функцию)

--- project.cpp
+++ project.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+
+
+vector <int> it_sort_vector(vector <int> mas){
+    int vrem;
+    for (int i = 0; i < mas.size() - 1; i++) {
+        for (int j = 0; j < mas.size() - i - 1; j++) {
+            if (mas[j] > mas[j + 1]) {
+                vrem = mas[j];
+                mas[j] = mas[j + 1];
+                mas[j + 1] = vrem;
+            }
+        }
+    }
+    return(mas);
+}
+
 int show_pow_two(int poww){
     int neww = 1;
     for (int i = 0; i < poww; i++)
